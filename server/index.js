dcbf0c8f9e408cc95dfce6b3cda5f3970a42e1eb
aFar1s <89140460+aFar1s@users.noreply.github.com>
2021-11-16 03:03:10 +0800

    create, get routes done and tested
    id specific to be done after sessions

--- server/index.js
+++ server/index.js
@@ -3,16 +3,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const session = require("express-session");
-const UserModel = require("./models/users")
+const UserModel = require("./models/users");
+const DashboardModel = require("./models/dashboard");
+const WalletModel = require("./models/wallet");
 
 // CONFIGURATION
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT ?? 4000;
-mongoose.connect(
-  process.env.MONGODB_URI ??
-    "mongodb://localhost/playground"
-);
+mongoose.connect(process.env.MONGODB_URI ?? "mongodb://localhost/playground", {
+  useNewUrlParser: true,
+});
 mongoose.connection.on("open", () => {
   console.log(
     `Connection to MongoDB ${process.env.MONGODB_URI ? "Atlas" : ""} is open`
@@ -34,37 +35,85 @@ app.use(
 
 //! Routes
 
-app.get("/users", (req, res) => {
-    UserModel.find({}, (err, result) => {
-        if (err) {
-            res.json(err);
-        } else {
-            res.json(result);
-        }
-    })
-})
-
-app.post("/registerUser", async (req, res) => {
-    const user = req.body
-    const newUser = new UserModel(user)
-    await newUser.save()
-
-    res.json()
-})
-
 // User Routes
 // const sessionController = require("./controllers/session_controller");
 // app.use("/api/session", sessionController);
 // const userController = require("./controllers/user");
 // app.use("/api/user", userController);
 
-// // Dashboard Routes
+app.get("/users", (req, res) => {
+  UserModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+app.post("/registerUser", async (req, res) => {
+  const user = req.body;
+  const newUser = new UserModel(user);
+  await newUser.save();
+
+  res.json();
+});
+
+// Dashboard Routes
 // const dashboardController = require("./controllers/dashboard");
 // app.use("/api/dashboard", dashboardController);
 
-// // Wallet Routes
+// get
+app.get("/dashboard", (req, res) => {
+  DashboardModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+// Create Dashboard
+app.post("/registerDashboard", async (req, res) => {
+  const dashboard = req.body;
+  const newDashboard = new DashboardModel(dashboard);
+  await newDashboard.save();
+
+  res.json();
+});
+
+// Update Dashboard
+
+//
+
+// Wallet Routes
 // const walletController = require("./controllers/wallet");
 // app.use("/api/wallet", walletController);
+
+// Get
+app.get("/wallet", (req, res) => {
+  WalletModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+// Create Wallet
+app.post("/registerWalet", async (req, res) => {
+  const wallet = req.body;
+  const newWallet = new WalletModel(wallet);
+  await newWallet.save();
+
+  res.json();
+});
+
+// Update Wallet
+
+//
 
 // Listener
 app.listen(port, () => {
