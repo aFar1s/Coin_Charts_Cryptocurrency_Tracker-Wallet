da2ce451df64890b41700ad7874818e07f438369
aFar1s <89140460+aFar1s@users.noreply.github.com>
2021-11-15 10:16:32 +0800

    server updated
    route controller next

---
+++ server/index.js
@@ -0,0 +1,53 @@
+// DEPENDANCIES
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const session = require("express-session");
+
+// CONFIGURATION
+// require("dotenv").config();
+const app = express();
+const port = process.env.PORT ?? 4000;
+mongoose.connect(
+  process.env.MONGODB_URI ??
+    "mongodb+srv://afaris:afaris2127@firstcluster.r8zxu.mongodb.net/learn?authSource=admin&replicaSet=atlas-pe4a3c-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
+);
+mongoose.connection.on("open", () => {
+  console.log(
+    `Connection to MongoDB ${process.env.MONGODB_URI ? "Atlas" : ""} is open`
+  );
+});
+
+// Middleware
+// const path = require("path");
+// app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.json());
+app.use(cors());
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+//! Routes
+
+// User Routes
+const sessionController = require("./controllers/session_controller");
+app.use("/api/session", sessionController);
+const userController = require("./controllers/user");
+app.use("/api/user", userController);
+
+// Dashboard Routes
+const dashboardController = require("./controllers/dashboard");
+app.use("/api/dashboard", dashboardController);
+
+// Wallet Routes
+const walletController = require("./controllers/wallet");
+app.use("/api/wallet", walletController);
+
+// Listener
+app.listen(port, () => {
+  console.log(`Express server is live at ${port}`);
+});
