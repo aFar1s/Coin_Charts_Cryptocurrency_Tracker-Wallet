acc962ac03973c919632ca41fa6e0c327c3b54b5
aFar1s <89140460+aFar1s@users.noreply.github.com>
2021-11-17 00:50:50 +0800

    /controller wallet updated

--- server/controllers/wallet.js
+++ server/controllers/wallet.js
@@ -1,47 +1,84 @@
 const express = require("express");
 const router = express.Router();
-const WalletModel = require("../models/wallet");
+const Wallet = require("../models/wallet");
 
 // Get
 router.get("/", (req, res) => {
-  WalletModel.find({}, (err, result) => {
-    if (err) {
-      res.json(err);
-    } else {
-      res.json(result);
-    }
-  });
+  Wallet
+    .find()
+    .then((items) => res.json(items))
+    .catch((err) => res.status(400).json("Error: " + err))
 });
 
 // Create Wallet
-router.post("/registerWallet", async (req, res) => {
+router.post("/registerWallet", (req, res) => {
   const wallet = req.body;
-  const newWallet = new WalletModel(wallet);
-  await newWallet.save();
+  const newWallet = new Wallet(wallet);
 
-  res.json();
+  newWallet
+    .save()
+    .then((createdWallet) => console.log(createdWallet))
+    .catch((err) => res.status(400).json("Error " + err));
 });
 
 // Update Wallet
-router.put("/updateWallet", async (req, res) => {
-  const toUpdateCashTotal = req.body.newCashTotal;
-  const toUpdateCurrencyUnit = req.body.newCurrencyUnit;
-  const toUpdateCoinQuantity = req.body.newCoinQuantity;
-  const id = req.body.id;
+router.put("/updateWallet", (req, res) => {
+  const updatWallet = req.body;
 
-  try {
-    await WalletModel.findById(id, (error, walletToUpdate) => {
-      walletToUpdate.cashTotal = toUpdateCashTotal; // may have to convert to Number with "Number(toUpdateCashTotal)"
-      walletToUpdate.currencyUnit = toUpdateCurrencyUnit;
-      walletToUpdate.coinQuantity = toUpdateCoinQuantity;
-      walletToUpdate.save();
-    });
-  } catch (err) {
-    console.log(err);
-  }
-
-  res.json();
+  Wallet.findByIdAndUpdate(
+    { _id: req.body.id },
+    { $set: updatWallet },
+    (req, res, err) => {
+      if (!err) {
+        console.log("Item Updated!");
+      } else {
+        console.log(err);
+      }
+    }
+  )
+//   .then((updatedWallet) => res.json(updatedWallet))
+//   .then((updatedWallet) => console.log(updatedWallet))
+//   .catch((err) => res.status(400).json("Error " + err));
 });
-//
 
 module.exports = router;
+
+// Init Create
+// router.post("/registerWallet", async (req, res) => {
+//     const wallet = req.body;
+//     const newWallet = new WalletModel(wallet);
+//     await newWallet.save();
+
+//     res.json();
+//   });
+
+// init update
+// router.put("/updateWallet", async (req, res) => {
+//     const toUpdateCashTotal = req.body.newCashTotal;
+//     const toUpdateCurrencyUnit = req.body.newCurrencyUnit;
+//     const toUpdateCoinQuantity = req.body.newCoinQuantity;
+//     const id = req.body.id;
+
+//     try {
+//       await WalletModel.findById(id, (error, walletToUpdate) => {
+//         walletToUpdate.cashTotal = toUpdateCashTotal; // may have to convert to Number with "Number(toUpdateCashTotal)". Check with frontend.
+//         walletToUpdate.currencyUnit = toUpdateCurrencyUnit;
+//         walletToUpdate.coinQuantity = toUpdateCoinQuantity;
+//         walletToUpdate.save();
+//       });
+//     } catch (err) {
+//       console.log(err);
+//     }
+
+//     res.json();
+//   });
+
+// router.get("/", (req, res) => {
+//     Wallet.find({}, (err, result) => {
+//       if (err) {
+//         res.json(err);
+//       } else {
+//         res.json(result);
+//       }
+//     });
+//   });
