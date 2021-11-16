1b1b0eed60b32733b3b8b621b57fbd88d17ca68f
aFar1s <89140460+aFar1s@users.noreply.github.com>
2021-11-16 17:13:31 +0800

    put routes tested

--- server/controllers/wallet.js
+++ server/controllers/wallet.js
@@ -23,7 +23,25 @@ router.post("/registerWallet", async (req, res) => {
 });
 
 // Update Wallet
+router.put("/updateWallet", async (req, res) => {
+  const toUpdateCashTotal = req.body.newCashTotal;
+  const toUpdateCurrencyUnit = req.body.newCurrencyUnit;
+  const toUpdateCoinQuantity = req.body.newCoinQuantity;
+  const id = req.body.id;
 
+  try {
+    await WalletModel.findById(id, (error, walletToUpdate) => {
+      walletToUpdate.cashTotal = toUpdateCashTotal; // may have to convert to Number with "Number(toUpdateCashTotal)"
+      walletToUpdate.currencyUnit = toUpdateCurrencyUnit;
+      walletToUpdate.coinQuantity = toUpdateCoinQuantity;
+      walletToUpdate.save();
+    });
+  } catch (err) {
+    console.log(err);
+  }
+
+  res.json();
+});
 //
 
 module.exports = router;
