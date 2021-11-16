2653fd128205b4d58cac7c33a05d8b7176f0a166
aFar1s <89140460+aFar1s@users.noreply.github.com>
2021-11-17 02:53:44 +0800

    client initial

---
+++ client/src/Pages/Landing/Landing.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react'
+
+function Landing() {
+
+   
+
+
+
+
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Landing
