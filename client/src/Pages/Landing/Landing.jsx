ccd116b7785a877af487d6f682f42e33cae601bc
aFar1s <89140460+aFar1s@users.noreply.github.com>
2021-11-17 02:54:05 +0800

    Update Landing.jsx

--- client/src/Pages/Landing/Landing.jsx
+++ client/src/Pages/Landing/Landing.jsx
@@ -2,7 +2,17 @@ import React, { useState, useEffect } from 'react'
 
 function Landing() {
 
-   
+    const [coins, setCoins] = useState([]);
+    const [search, setSearch] = useState("");
+
+    const changeTextField = (e) => {
+        setSearch(e.target.value);
+      };
+
+    const filteredSearchString = coins.filter((coin) =>
+    coin.name.toLowerCase()
+    .includes(search.toLowerCase())
+    );
 
 
 
