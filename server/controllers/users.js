16aaa5e0609d5dc0ee41e8d6152b197f91dc401d
aFar1s <89140460+aFar1s@users.noreply.github.com>
2021-11-18 06:17:55 +0800

    Update users.js

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -18,6 +18,7 @@ router.post("/registerUser", async (req, res, next) => {
   const { name, email, password, dateOfBirth } = req.body;
 
   try {
+    // Create User
     const user = await User.create({
       name,
       email,
@@ -25,9 +26,11 @@ router.post("/registerUser", async (req, res, next) => {
       dateOfBirth,
     });
 
+    // Create Dashboard
     const dashboard = new Dashboard({ owner: user._id });
     await dashboard.save();
 
+    // Create Wallet
     const wallet = new Wallet({ owner: user._id });
     await wallet.save();
 
@@ -42,6 +45,51 @@ const sendToken = (user, statusCode, res) => {
   const token = user.getSignedJwtToken();
   res.status(statusCode).json({ sucess: true, token });
 };
+
+
+router.post("/userLogin", async (req, res, next) => {
+
+  const { email, password } = req.body;
+
+  // Check if email and password is provided
+  if (!email || !password) {
+    return next(new ErrorResponse("Please provide an email and password", 400));
+  }
+
+  try {
+    // Check that user exists by email
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return next(new ErrorResponse("Invalid credentials", 401));
+    }
+
+    // Check that password match
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      return next(new ErrorResponse("Invalid credentials", 401));
+    }
+
+    sendToken(user, 200, res);
+  } catch (err) {
+    next(err);
+  }
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
 
 module.exports = router;
 
