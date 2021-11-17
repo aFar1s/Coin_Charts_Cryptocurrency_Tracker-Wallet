f30fda98e7228e95f1baf537a9f60397b8cda96b
aFar1s <89140460+aFar1s@users.noreply.github.com>
2021-11-18 04:05:35 +0800

    /models users. hash password fx

--- server/models/users.js
+++ server/models/users.js
@@ -3,32 +3,44 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-const usersSchema = new mongoose.Schema({
-  email: { 
-    type: String, 
-    require: [true, "Please enter username"], 
+const UsersSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    require: [true, "Please enter username"],
     unique: true,
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      "Please enter a valid email address"
-    ]
+      "Please enter a valid email address",
+    ],
   },
-  name: { 
-    type: String, 
-    require: [true, "Please enter name"]
+  name: {
+    type: String,
+    require: [true, "Please enter name"],
   },
-  password: { 
-    type: String, 
+  password: {
+    type: String,
     require: [true, "Password required"],
     minlength: 6,
-    select: false 
+    select: false,
   },
-  dateOfBirth: { 
-    type: Date, 
-    require: [true, "Please verify age"] 
+  dateOfBirth: {
+    type: Date,
+    require: [true, "Please verify age"],
   },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
 });
 
-const Users = mongoose.model("Users", usersSchema);
+UsersSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+const Users = mongoose.model("Users", UsersSchema);
 
 module.exports = Users;
