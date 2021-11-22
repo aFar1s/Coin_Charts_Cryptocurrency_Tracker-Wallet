e80dae0413a9e2a6df707a7f99e8e96dbdbb88f3
aFar1s <89140460+aFar1s@users.noreply.github.com>
2021-11-23 05:54:55 +0800

    /register logic

--- client/src/Components/screens/RegisterScreen.jsx
+++ client/src/Components/screens/RegisterScreen.jsx
@@ -3,13 +3,52 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import "./RegisterScreen.css";
 
-const RegisterScreen = () => {
+const RegisterScreen = ({history}) => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmpassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
 
+  const registerHandler = async (e) => {
+    e.preventDefault();
+
+    const config = {
+      header: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    if (password !== confirmpassword) {
+      setPassword("");
+      setConfirmPassword("");
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+      return setError("Passwords do not match");
+    }
+
+    try {
+      const { data } = await axios.post(
+        "/api/auth/register",
+        {
+          username,
+          email,
+          password,
+        },
+        config
+      );
+
+      localStorage.setItem("authToken", data.token);
+
+      history.push("/");
+    } catch (error) {
+      setError(error.response.data.error);
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+    }
+  };
 
 
 
