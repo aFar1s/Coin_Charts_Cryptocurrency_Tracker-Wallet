0e4fd9a54a832c5ffbb1bdc858262193db7ed5f5
aFar1s <89140460+aFar1s@users.noreply.github.com>
2021-11-23 05:53:07 +0800

    /RegisterScreen set states

--- client/src/Components/screens/RegisterScreen.jsx
+++ client/src/Components/screens/RegisterScreen.jsx
@@ -2,3 +2,19 @@ import { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import "./RegisterScreen.css";
+
+const RegisterScreen = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmpassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+
+
+
+
+
+}
+
+export default RegisterScreen;
