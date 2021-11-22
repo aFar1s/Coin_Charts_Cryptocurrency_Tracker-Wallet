d4e206daa8a172d9bc44a2a29b9de3dbdb921ec1
aFar1s <89140460+aFar1s@users.noreply.github.com>
2021-11-22 10:27:51 +0800

    mui grid implement

--- client/src/Pages/Dashboard/Dashboard.jsx
+++ client/src/Pages/Dashboard/Dashboard.jsx
@@ -3,8 +3,9 @@ import React, { useState } from 'react'
 import CoinSearch from './components/CoinSearch'
 import Chart1 from "./components/Chart1"
 import Chart2 from "./components/Chart2"
-import Grid from "@material-ui/core/Grid"
-import Paper from "@material-ui/core/Paper"
+import Grid from '@mui/material/Grid'
+import Paper from '@mui/material/Paper'
+import "./dashboard.css"
 
 const Dashboard = () => {
     const [chartCoin, setChartCoin] = useState("bitcoin")
@@ -16,24 +17,42 @@ const Dashboard = () => {
 
 
         <div>
+         <Grid container>
+             <Grid item sm={12} md={6}>
           <div className="dash-top-right">
+              <Paper>   
               <h1>COIN DATA</h1>
+              </Paper>
           </div>
+          </Grid>
+          <Grid item sm={12} md={6}>
           <div className="dash-top-left">
+          <Paper>
               <h1>Chart 1</h1>
               <h2>{chartCoin} Prices</h2>
               <Chart1 chartCoin={chartCoin} />
+              </Paper>
           </div>
+          </Grid>
+          <Grid item sm={12} md={6}>
           <div className="dash-bot-right">
+          <Paper>
               <h1>Chart 2</h1>
               <h2>{chartCoin} Trading Volume</h2>
               <Chart2 chartCoin={chartCoin} />
+              </Paper>
           </div>
+          </Grid>
+          <Grid item sm={12} md={6}>
           <div className="dash-bot-left">
+          <Paper>
               <CoinSearch 
               setChartCoin={setChartCoin}
               />
-          </div>   
+              </Paper>
+          </div>
+          </Grid>
+         </Grid>   
         </div>
     )
 }
