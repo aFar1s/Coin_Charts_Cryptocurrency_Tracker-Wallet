088e5c8674a404eed660c19f3ed9508166baea41
aFar1s <89140460+aFar1s@users.noreply.github.com>
2021-11-26 11:56:27 +0800

    css

--- client/src/Pages/Dashboard/components/DashboardCoin.jsx
+++ client/src/Pages/Dashboard/components/DashboardCoin.jsx
@@ -5,26 +5,26 @@ const DashboardCoin = ({ coinId ,name, price, symbol, marketcap, volume, image, 
     return (
       <div className='coin-container'>
        <Link to="/dashboard" onClick={() => {console.log(coinId); setChartCoin(coinId)}}>
-      <div className='coin-row'>
-        <div className='coin'>
+      <div className='dash-coin-row'>
+        <div className='dash-coin'>
           <img src={image} alt='crypto' />
           <h1>{name}</h1>
           {/* <p className='coin-symbol'>{symbol}</p> */}
         </div>
-        <div className='coin-data'>
-          <p className='coin-price'>${price}</p>
+        {/* <div className='coin-data'> */}
+          {/* <p className='coin-price'>${price}</p> */}
           {/* <p className='coin-volume'>${volume.toLocaleString()}</p> */}
           {/* <p className='coin-marketcap'>
             Mkt Cap: ${marketcap.toLocaleString()}
           </p> */}
 
-          {priceChange < 0 ? (
+          {/* {priceChange < 0 ? (
             <p className='coin-percent red'>{priceChange.toFixed(2)}%</p>
           ) : (
             <p className='coin-percent green'>{priceChange.toFixed(2)}%</p>
           )}
 
-        </div>
+        </div> */}
       </div>
        </Link>
     </div>
