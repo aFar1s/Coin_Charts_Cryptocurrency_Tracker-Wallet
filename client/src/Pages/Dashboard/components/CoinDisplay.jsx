483ef2949ed82574af5fa47baecc5a06b1b06b21
aFar1s <89140460+aFar1s@users.noreply.github.com>
2021-11-26 08:23:28 +0800

    Update CoinDisplay.jsx

--- client/src/Pages/Dashboard/components/CoinDisplay.jsx
+++ client/src/Pages/Dashboard/components/CoinDisplay.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function CoinDisplay() {
 
+    const [coinData, setCoinData] = useState([])
 
+    useEffect(() => {
+        axios
+          .get(
+            'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+          )
+          .then(res => {
+            setCoinData(res.data);
+            console.log(res.data);
+          })
+          .catch(error => console.log(error));
+      }, []
+    );
 
-
-
+    
+    
     return (
         <div>
+            {coinData.map(coin => {
+                return (
+                    <div>
+                    <h3></h3>
+                    <h3></h3>
+                    <h3></h3>
+                    <h3></h3>
+                    <h3></h3>
+                    <h3></h3>
+                    <h3></h3>
+                    <h3></h3>
+                    <h3></h3>
+                    <h3></h3>
+                    </div>
+                )
+            })}
             
         </div>
     )
