58c91b1fa25a4866f75d1c18f065aef9a402fd1a
aFar1s <89140460+aFar1s@users.noreply.github.com>
2021-12-22 18:28:49 +0800

    execute validation done

--- client/src/Pages/Wallet/components/BusSellScreen.jsx
+++ client/src/Pages/Wallet/components/BusSellScreen.jsx
@@ -35,10 +35,15 @@ const BusSellScreen = ({
 }) => {
   const [quantity, setQuantity] = useState(1);
   const [buySell, setBuySell] = useState(null);
+  const [validationText, setValidationText] = useState("");
 
-  const coinValueInWallet = coinValue_quantity(quantity)
+  const coinValueInWallet = coinValue_quantity(quantity);
   const ownerID = sessionStorage.getItem("userID");
 
+  const validationTextChange = (event) => {
+    setValidationText(event.target.value);
+    console.log(validationText);
+  };
 
   const handleQuantityChange = (event) => {
     event.preventDefault();
@@ -58,20 +63,55 @@ const BusSellScreen = ({
     }
     setQuantity(1);
     setBuySell(null);
+    setValidationText("")
   };
 
   const handleExecute = () => {
     setOpen(false);
-    const updatedCashBalance = buySell === "buy" ? (cashBalance - coinValueInWallet) : (cashBalance + coinValueInWallet)
-    const updatedWalletQuantity = buySell === "buy" ? (walletQuantity + quantity) : (walletQuantity - quantity)
-  
+    const updatedCashBalance =
+      buySell === "buy"
+        ? cashBalance - coinValueInWallet
+        : cashBalance + coinValueInWallet;
+    const updatedWalletQuantity =
+      buySell === "buy" ? walletQuantity + quantity : walletQuantity - quantity;
+
     if (buySell === "sell" && updatedWalletQuantity === 0) {
       axios.delete(`api/wallet/delete/${walletID}`).catch((err) => {
-          console.error(err);
+        console.error(err);
       });
 
       axios
-      .put(`/api/cashWallet/updateCash/${ownerID}`, { cashTotal: updatedCashBalance })
+        .put(`/api/cashWallet/updateCash/${ownerID}`, {
+          cashTotal: updatedCashBalance,
+        })
+        .then((res) => {
+          console.log(res.data);
+          setWalletStateToggle(!walletStateToggle);
+          console.log(`New cash balance is ${res.data.cashTotal}`);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+
+    axios
+      .put(`/api/wallet/updateWallet/${walletID}`, {
+        quantity: updatedWalletQuantity,
+      })
+      .then((res) => {
+        console.log(res.data);
+        setWalletStateToggle(!walletStateToggle);
+        setBuySell(null);
+        console.log(`New ${coinName} total is ${res.data.quantity}`);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    axios
+      .put(`/api/cashWallet/updateCash/${ownerID}`, {
+        cashTotal: updatedCashBalance,
+      })
       .then((res) => {
         console.log(res.data);
         setWalletStateToggle(!walletStateToggle);
@@ -80,61 +120,44 @@ const BusSellScreen = ({
       .catch((error) => {
         console.log(error);
       });
-    }
-
-    axios
-      .put(`/api/wallet/updateWallet/${walletID}`, { quantity: updatedWalletQuantity })
-      .then((res) => {
-        console.log(res.data);
-        setWalletStateToggle(!walletStateToggle);
-        setBuySell(null);
-        console.log(`New ${coinName} total is ${res.data.quantity}`);
-    })
-      .catch((error) => {
-        console.log(error);
-    });
-
-    axios
-      .put(`/api/cashWallet/updateCash/${ownerID}`, { cashTotal: updatedCashBalance })
-      .then((res) => {
-        console.log(res.data);
-        setWalletStateToggle(!walletStateToggle);
-        console.log(`New cash balance is ${res.data.cashTotal}`);
-    })
-      .catch((error) => {
-        console.log(error);
-    });
     console.log(buySell);
   };
 
   const valueInfo =
-  buySell === "buy" && coinValueInWallet > cashBalance ? (
-    <h3>Input a lower quantity or click cancel to choose another Coin</h3>
-  ) : buySell === "buy" ? (
-    <h4>${coinValueInWallet} will be deducted from you Cash Balance.</h4>
-  ) : (
-    <h4>${coinValueInWallet} will be added to your Cash Balance.</h4>
-  );
+    buySell === "buy" && coinValueInWallet > cashBalance ? (
+      <h3>Input a lower quantity or click cancel to choose another Coin</h3>
+    ) : buySell === "buy" ? (
+      <h4>${coinValueInWallet} will be deducted from you Cash Balance.</h4>
+    ) : (
+      <h4>${coinValueInWallet} will be added to your Cash Balance.</h4>
+    );
   const valueStatement = () => {
     if (buySell === null) {
       return;
     } else return valueInfo;
   };
-  
+
   const buyInfo =
-  buySell === "buy" && coinValueInWallet > cashBalance ? (
-    <h3>Attempted purchase ${coinValueInWallet} of {upperCase(coinName)} exceeds available Cash Balance of ${cashBalance}</h3>
-  ) : buySell === "buy" ? (
-    <h3>You are purchasing {quantity} {coinName}.</h3>
-  ) : (
-    <h3>You are selling {quantity} {coinName}</h3>
-  );
+    buySell === "buy" && coinValueInWallet > cashBalance ? (
+      <h3>
+        Attempted purchase ${coinValueInWallet} of {upperCase(coinName)} exceeds
+        available Cash Balance of ${cashBalance}
+      </h3>
+    ) : buySell === "buy" ? (
+      <h3>
+        You are purchasing {quantity} {coinName}.
+      </h3>
+    ) : (
+      <h3>
+        You are selling {quantity} {coinName}
+      </h3>
+    );
   const buySatement = () => {
     if (buySell === null) {
       return;
     } else return buyInfo;
   };
-  
+
   const buySellInfo =
     buySell === "buy" ? (
       <h3>Select Amount to Buy</h3>
@@ -147,7 +170,7 @@ const BusSellScreen = ({
     } else return buySellInfo;
   };
 
-  let sellArray = []
+  let sellArray = [];
   for (let index = 1; index <= walletQuantity; index++) {
     sellArray.push(index);
   }
@@ -156,21 +179,22 @@ const BusSellScreen = ({
     buyArray.push(index);
   }
 
-  const buySellArray = buySell === "buy" ?
-  (buyArray.map((buy) => (
-    <MenuItem key={buy} value={buy}>
-      {buy}
-    </MenuItem>
-  )))
-  :
-  (sellArray.map((sell) => (
-    <MenuItem key={sell} value={sell}>
-        {sell}
-    </MenuItem>  
-  )))
+  const buySellArray =
+    buySell === "buy"
+      ? buyArray.map((buy) => (
+          <MenuItem key={buy} value={buy}>
+            {buy}
+          </MenuItem>
+        ))
+      : sellArray.map((sell) => (
+          <MenuItem key={sell} value={sell}>
+            {sell}
+          </MenuItem>
+        ));
 
-  const disableExecute = buySell === "buy" ? (coinValueInWallet > cashBalance) : (false)
-  
+  const disableExecute =
+    buySell === "buy" ? coinValueInWallet > cashBalance : false;
+
   return (
     <div className="coin-popup">
       <Dialog disableEscapeKeyDown open={open} onClose={handleClose}>
@@ -190,11 +214,7 @@ const BusSellScreen = ({
                 name="radio-buttons-group"
                 onChange={handleRadio}
               >
-                <FormControlLabel 
-                  value="buy" 
-                  control={<Radio />} 
-                  label="Buy" 
-                />
+                <FormControlLabel value="buy" control={<Radio />} label="Buy" />
                 <FormControlLabel
                   value="sell"
                   control={<Radio />}
@@ -225,10 +245,34 @@ const BusSellScreen = ({
           {valueStatement()}
         </DialogContent>
         <DialogActions>
+          {buySell !== null ? (
+            <div>
+              <h5 style={{ marginLeft: "15px", marginRight: "20px" }}>
+                Type <bold>"{buySell} {coinName}"</bold> to validate transaction
+              </h5>
+              <input
+                type="text"
+                value={validationText}
+                placeholder="Enter validation..."
+                onChange={validationTextChange}
+                style={{ marginRight: "20px", marginLeft: "15px", marginBottom: "10px" }}
+              />
+            </div>
+          ) : (
+            <div></div>
+          )}
           <Button variant="outlined" onClick={handleClose}>
             Cancel
           </Button>
-          <Button variant="outlined" disabled={disableExecute || buySell === null} onClick={handleExecute}>
+          <Button
+            variant="outlined"
+            disabled={
+              disableExecute ||
+              buySell === null ||
+              validationText !== `${buySell} ${coinName}`
+            }
+            onClick={handleExecute}
+          >
             Execute
           </Button>
         </DialogActions>
@@ -238,7 +282,3 @@ const BusSellScreen = ({
 };
 
 export default BusSellScreen;
-
-
-
-
